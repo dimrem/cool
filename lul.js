5295269152e48d732b7c31d7ba8c7c098244fcf4
dimrem <thecastrad@gmail.com>
2017-03-07 00:24:28 +0700

    Update lul.js

--- lul.js
+++ lul.js
@@ -21,11 +21,8 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="redirect-url-5.html"}
-if(x=="2"){xxx="redirect-url-4.html"}
-if(x=="3"){xxx="redirect-url-3.html"}
-if(x=="4"){xxx="redirect-url-2.html"}
-if(x=="5"){xxx="redirect-url.html"}a_to_ve[i].href="https://shortenlinkid.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+
+if(x=="1"){xxx="redirect-url.html"}a_to_ve[i].href="https://shortenlinkid.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
